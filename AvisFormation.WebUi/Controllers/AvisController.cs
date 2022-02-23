afae908e8b8354d269f66020097d05bf9f0e1721
chris241 <chrisleon325@gmail.com>
2022-02-23 08:19:15 +0300

    ajout model laisserUnAvisViewModel

--- AvisFormation.WebUi/Controllers/AvisController.cs
+++ AvisFormation.WebUi/Controllers/AvisController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AvisFormation.Data;
+using AvisFormation.WebUi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,44 @@ namespace AvisFormation.WebUi.Controllers
         // GET: Avis
         public ActionResult LaisserUnAvis(string nomSeo)
         {
+            var vm = new LaisserUnAvisViewModel();
+            vm.NomSeo = nomSeo;
+            using (var context = new AvisEntities())
+            {
+                var formationActuelle = context.Formation.FirstOrDefault(f => f.NomSeo == nomSeo);
+                if (formationActuelle == null)
+                {
+                    return RedirectToAction("Accueil", "Home");
+                }
+                vm.FormationName = formationActuelle.Nom;
+            }
+                return View(vm);
+        }
+        public ActionResult SaveComment(string commentaire, string nom, string note, string nomSeo)
+        {
+            Avis nouvelAvis = new Avis();
+            nouvelAvis.DateAvis = DateTime.Now;
+            nouvelAvis.Description = commentaire;
+     
+            nouvelAvis.Nom = nom;
+            Byte bNote = 0;
+            if(!Byte.TryParse(note, out bNote))
+            {
+                throw new Exception("Impossible de parser la note " + note);
+            }
+            nouvelAvis.Note = bNote;
+            using( var context = new AvisEntities())
+            {
+                var formationActuelle = context.Formation.FirstOrDefault(f => f.NomSeo == nomSeo);
+                if(formationActuelle == null)
+                {
+                    return RedirectToAction("Accueil", "Home");
+                }
+                nouvelAvis.IdFormation = formationActuelle.Id;
+                context.Avis.Add(nouvelAvis);
+                context.SaveChanges();
+
+            }
             return View();
         }
     }
